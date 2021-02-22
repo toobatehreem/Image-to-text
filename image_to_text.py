eaf28fdfd4b3ffdf671110a9d9c0e4c6a3d22311
Tooba Tehreem Sheikh <51713056+toobatehreem@users.noreply.github.com>
2021-02-22 16:56:43 +0500

    Create image_to_text.py

---
+++ image_to_text.py
@@ -0,0 +1,9 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"" #install pytesseract module and include the downloaded file path within the inverted commas like "C:/Users/abc/pytesseract/pytesseract.py"
+
+img = Image.open("") #enter the image filename like "testimage.jpg"
+text = pytesseract.image_to_string(img)
+text1 = pytesseract.image_to_string(img)
+print(text1)
