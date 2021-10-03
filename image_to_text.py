65160c8ee3a79842d6f147f12f23a0987784e945
Tooba Tehreem Sheikh <51713056+toobatehreem@users.noreply.github.com>
2021-10-03 12:05:13 +0500

    Path correction

--- image_to_text.py
+++ image_to_text.py
@@ -1,8 +1,8 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"" #install pytesseract module and include the downloaded file path within the inverted commas like "C:/Users/abc/pytesseract/pytesseract.py"
+pytesseract.pytesseract.tesseract_cmd = r"" #install pytesseract module and include the downloaded file path within the inverted commas like "C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
-img = Image.open("") #enter the image filename like "testimage.jpg"
+img = Image.open("") #enter the image filename as "testimage.jpg"
 text = pytesseract.image_to_string(img)
 print(text)
